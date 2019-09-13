12fdb860f85016f6146ea9d14cb4594b5e0a7036
Matthew <mkleiner2670@gmail.com>
2019-09-13 00:45:29 -0400

    Updates

---
+++ precache-manifest.59f12a0a33cd2f50a34513fcdab420e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92926b94e80f0ac0e3ff619378e05f6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93d49895fe8a21426b2c",
+    "url": "/static/css/main.385eed2e.chunk.css"
+  },
+  {
+    "revision": "ae2022d50b0abe3cd376",
+    "url": "/static/js/2.40db250d.chunk.js"
+  },
+  {
+    "revision": "93d49895fe8a21426b2c",
+    "url": "/static/js/main.0c4d3f36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45c1d90b0e29fd5b6298d41ed6dd0927",
+    "url": "/static/media/chopchamps.45c1d90b.png"
+  },
+  {
+    "revision": "8973f7162f88b9e6ae15f403c2e4cd5f",
+    "url": "/static/media/cpu.8973f716.png"
+  },
+  {
+    "revision": "11daaffe7d1c73508315488068eee1ff",
+    "url": "/static/media/machina.11daaffe.png"
+  }
+]);
